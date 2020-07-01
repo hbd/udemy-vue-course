660483d571b62fb1a325deacfc4bea8ca23bebb8
Zak <zak.keener@getbellhops.com>
2020-07-01 07:45:19 -0700

    Use v-for to interate through data collections

--- lessons/app.js
+++ lessons/app.js
@@ -3,17 +3,13 @@ new Vue({
     el: '#app', // Just a CSS selector (on id)
     data: {
         title: 'Becoming a Vue Samurai',
-        name: 'Zak',
-        age: 25,
-        showName: false,
-        showAge: true
+        items: ['Mushroom', 'Green Shells', 'Banana', 'Start'],
+        ninjas: [
+            { name: 'Crystal', age: 25, beltColor: 'black' },
+            { name: 'Ryu', age: 30, beltColor: 'brown' },
+            { name: 'Ken', age: 35, beltColor: 'orange' }
+        ]
     },
     methods: {
-        toggleName() {
-            this.showName = !this.showName
-        },
-        toggleAge() {
-            this.showAge = !this.showAge
-        }
     }
 })
