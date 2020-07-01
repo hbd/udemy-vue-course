4737f8101100fbc7b88de748878a00b2ed481f0f
Zak <zak.keener@getbellhops.com>
2020-07-01 07:46:41 -0700

    Commit basic template

--- lessons/app.js
+++ lessons/app.js
@@ -2,13 +2,6 @@
 new Vue({
     el: '#app', // Just a CSS selector (on id)
     data: {
-        title: 'Becoming a Vue Samurai',
-        items: ['Mushroom', 'Green Shells', 'Banana', 'Start'],
-        ninjas: [
-            { name: 'Crystal', age: 25, beltColor: 'black' },
-            { name: 'Ryu', age: 30, beltColor: 'brown' },
-            { name: 'Ken', age: 35, beltColor: 'orange' }
-        ]
     },
     methods: {
     }
