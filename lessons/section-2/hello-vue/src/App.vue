5b3513964b500c62dd64da7a2bd6c68856d4848e
Zak <zak.keener@getbellhops.com>
2020-07-01 19:31:43 -0700

    Add *Friend and Navbar components

--- lessons/section-2/hello-vue/src/App.vue
+++ lessons/section-2/hello-vue/src/App.vue
@@ -1,30 +1,46 @@
+<!-- this is the template we want to render -->
+<!-- only one root element allowed per Vue file/template -->
+<!-- examples of components: login/auth, body, header, footer, etc -->
 <template>
+  <!-- root HTML element -->
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1 id="app-title">{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends="friends" @delete="deleteFriend"/>
+    <OnlineFriends :friends="friends"/>
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+
+// This is a component.
 export default {
   name: 'app',
+  components: {
+    Navbar, // w/o ES6, would need to be: Navbar: Navbar
+    AllFriends,
+    OnlineFriends
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'Hello, Vue!',
+      friends: [
+          { name: 'Mario', online: true },
+          { name: 'Luigi', online: false },
+          { name: 'Toad', online: true },
+          { name: 'Bowser', online: false }
+      ]
+    }
+  },
+  methods: {
+    deleteFriend(e) {
+      // Return friends that are not the given friend.
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== e.name
+      })
     }
   }
 }
@@ -35,26 +51,14 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
 
-h1, h2 {
+h1 {
+  color: #444;
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  text-align: center;
 }
 </style>
--- lessons/section-2/hello-vue/src/main.js
+++ lessons/section-2/hello-vue/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: '#app',
+  // Control the HTML element with id = "app"
+  el: '#app', // Which element do we want this instance to control?
   render: h => h(App)
 })
